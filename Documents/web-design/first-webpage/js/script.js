0734812f9a461ae09ca18203d1a628effdf77380
Altynaim6 <altynaimunduzbaeva@gmail.com>
2024-02-19 01:01:12 +0600

    first commit

---
+++ Documents/web-design/first-webpage/js/script.js
@@ -0,0 +1,26 @@
+// const myHeading = document.querySelector("h1");
+// myHeading.textContent = "Hello, World!";
+
+const imgElement = document.querySelector("img");
+
+const img1URL = "img/Ala-Too.png";
+const img2URL = "img/COMSEP-23.png";
+
+function changePhoto(event){
+    let current = imgElement.getAttribute("src");
+    if(current == img1URL){
+        imgElement.setAttribute("src",img2URL);
+    } else {
+        imgElement.setAttribute("src", img1URL)
+    }
+}
+
+imgElement.onclick = changePhoto;
+
+document.getElementById("change-name").addEventListener("click", function() {
+    var name = prompt("Please enter your name:");
+    if (name != null) {
+        document.getElementById("header").innerText = "Welcome " + name + "!";
+    }
+});
+
